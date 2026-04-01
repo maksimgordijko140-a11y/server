98274d9a656ce13e14680901b69bb2d25f748ccf
Wyiorlm <maksimgordijko140@gmail.com>
2026-04-01 18:02:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 3000
 const cors = require("cors")
-app.use(cors)
+app.use(cors())
 
 app.use(express.json());
 const jsonParcer = express.json()
